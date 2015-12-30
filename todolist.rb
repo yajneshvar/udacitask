9ca7515b6937dc8c037d6547dbb9100ab6f38332
yaj <yajneshvar.arumugam@gmail.com>
2015-12-30 15:03:51 -0500

    completed add items and print methods for todolist

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,58 @@
 class TodoList
     # methods and stuff go here
+    attr_reader :title, :items
+
+    def initialize (title_name)
+        @title = title_name
+        @items = Array.new
+    end
+
+    def add_item(*new_item)
+        new_item.each do |item| 
+          entry  = Item.new(item)
+          @items << entry
+        end
+    end
+
+    def remove_item(item_name)
+        @items.delete_if{|item| item.description == item_name}
+    end
+
+    def update_item_completed(item_name)
+        index = @items.find_index {|item| item.description == item_name} 
+        @items[index].update_status(true)
+    end
+
+    def print_list
+        puts "---------------------"
+        puts @title
+        puts "----------------------"
+
+        @items.each do |item|
+            item.print_item
+        end
+    end
+    
+
 end
 
 class Item
-    # methods and stuff go here
+    attr_accessor :id
+    attr_reader :description, :completed_status
+
+    def initialize(item_description)
+        @description = item_description
+        @completed_status = false
+        @id = 0
+    end
+
+    def update_status(status)
+        @completed_status = status
+    end
+
+    def print_item
+       puts "#{@id}. #{@description} #{@completed_status ? "completed" : "uncompleted"}" 
+    end
 end
+
+
--- udacitask.rb
+++ udacitask.rb
@@ -1,11 +1,12 @@
 require './todolist.rb'
 
 # Creates a new todo list
-
+list = TodoList.new("Yaj's to do list")
 # Add four new items
+list.add_item("Buy groceries","Meetup with Karan","Check on sis","Finish anthropology")
 
 # Print the list
-
+list.print_list
 # Delete the first item
 
 # Print the list
